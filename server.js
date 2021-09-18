bf80a93fca57351080ca1f501d7227b2904fc7e2
Shamar Malcolm <70509256+marko-a87@users.noreply.github.com>
2021-09-18 17:14:25 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+// Connect Db
+
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  res.send("Api running");
+});
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/private", require("./routes/private"));
+
+//Error Handler (Should be last piece of middleware)
+app.use(errorHandler);
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Logged Error: ${err}`);
+  server.close(() => process.exit(1));
+});
